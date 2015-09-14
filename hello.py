abba111300ad08a5b5241bd9f10475631b186b10
daichengwei <joedai@umich.edu>
2015-09-14 09:43:20 +0800

    database version

--- hello.py
+++ hello.py
@@ -1,9 +1,18 @@
 from flask import Flask
+from flask.ext.mysqldb import MySQL
+
 app = Flask(__name__)
+mysql = MySQL()
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'DHGLYjoe521'
+mysql.init_app(app)
 
 @app.route("/helloworld")
 def hello():
-    return "Hello World!"
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * from helloworld.messages')
+    msgs = cur.fetchall()
+    return str(msgs)
 
 if __name__ == "__main__":
     app.run()
